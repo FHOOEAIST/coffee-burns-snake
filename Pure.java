651f4023d4842e118cb906b3c03134d8cc48671d
Andreas Pointner <andreas.pointner@fh-hagenberg.at>
2021-03-24 10:49:45 +0100

    #6 Added initial project version.

---
+++ Pure.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2021 the original author or authors.
+ * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+public class Pure {
+    public static void main(String[] args) {
+        int warmup = 100;
+        int runs = 1000;
+
+        int width = 1920;
+        int height = 1080;
+        int channels = 1;
+        boolean normalize = true;
+
+        int kernelSize = 5;
+        double[][] averageKernel = new double[kernelSize][kernelSize];
+        for (int x = 0; x < kernelSize; x++) {
+            for (int y = 0; y < kernelSize; y++) {
+                averageKernel[x][y] = 1.0 / ((double) (kernelSize * kernelSize));
+            }
+        }
+
+        short[][][] image = new short[height][width][channels];
+
+        short cnt = 0;
+        for (int x = 0; x < width; x++) {
+            for (int y = 0; y < height; y++) {
+                for (int c = 0; c < channels; c++) {
+                    image[y][x][c] = cnt++;
+
+                    if (cnt > 255) {
+                        cnt = 0;
+                    }
+                }
+            }
+        }
+
+        for (int r = 0; r < runs + warmup; r++) {
+            long before = System.currentTimeMillis();
+            short[][][] result = new short[height][width][channels];
+
+
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    for (int c = 0; c < channels; c++) {
+                        double sum = 0.0;
+                        double maskSum = 0.0;
+                        int yRadius = (kernelSize - 1) / 2;
+                        for (int yOffset = -yRadius; yOffset <= yRadius; yOffset++) {
+                            int xRadius = (kernelSize - 1) / 2;
+                            for (int xOffset = -xRadius; xOffset <= xRadius; xOffset++) {
+                                int nbX = x + xOffset;
+                                int nbY = y + yOffset;
+                                if ((nbX >= 0) && (nbX < width) && (nbY >= 0) && (nbY < height)) {
+                                    sum += result[nbY][nbX][0] * averageKernel[yOffset + yRadius][xOffset + xRadius];
+                                    maskSum += averageKernel[yOffset + yRadius][xOffset + xRadius];
+                                } //range check
+                            } // xOffset
+                        } // yOffset
+                        if (normalize) {
+                            if (maskSum == 0) {
+                                throw new IllegalStateException("KernelSum is 0 but normalizing is active");
+                            }
+                            sum *= 1.0 / maskSum;
+                        } // normalize
+
+                        result[y][x][c] = (short) sum;
+                    }
+                }
+            }
+
+            long after = System.currentTimeMillis();
+            if(r > warmup - 1){
+                System.out.println("" + (after - before));
+            }
+        }
+    }
+}
---
+++ run_with_numpy_and_scipy.py
@@ -0,0 +1,55 @@
+import pickle
+import timeit
+from itertools import product
+
+import cv2 as cv
+import numba
+import numpy as np
+from numba.typed import List as nList
+from scipy import ndimage
+
+
+def fix_array_or_list(matrix):
+    if isinstance(matrix, np.ndarray):
+        return matrix
+
+    if not matrix:
+        raise ValueError("List must contain at least one element")
+
+    new_list = nList()
+    for r in matrix:
+        new_list.append(r)
+    return new_list
+
+
+def run_with_numpy_and_scipy(matrix: list, h: int, w: int, kernel: int = 3):
+    _matrix = np.asarray(matrix)
+    conv = np.ones((kernel, kernel), dtype=int) / (kernel * kernel)
+    return ndimage.convolve(_matrix, conv, mode='constant', cval=0)
+
+
+def run_timed(func, matrix: list, h: int, w: int, warmpup:int = 100, measurement:int = 1000, kernel: int = 3):
+    # warmup
+    warm_up_times = timeit.repeat(lambda: func(matrix, h, w, kernel), number=1, repeat=warmpup)
+    run_times = timeit.repeat(lambda: func(matrix, h, w, kernel), number=1, repeat=measurement)
+    print(f"WarmUp: {warm_up_times}")
+    for x in run_times:
+        print(str(x*1000))
+
+
+if __name__ == '__main__':
+    width = 1920  # 20  # 1920
+    height = 1080  # 20  # 1080
+    kernel_size = 5
+    arr = list(np.zeros((height, width)).tolist())
+    v = 0
+    for r in range(0, height):
+        for c in range(0, width):
+            arr[r][c] = v
+            v += 1
+            v %= 255
+    n_arr = fix_array_or_list(arr)
+    np_arr = np.asarray(arr)
+    np_uint_arr = np.asarray(np_arr, dtype=np.uint8)
+
+    run_timed(run_with_numpy_and_scipy, np_arr, height, width, 100, 1000, kernel_size)
